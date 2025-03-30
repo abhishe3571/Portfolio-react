eb85ef6fa2788f3cc32ffb10a031e338a79d89b8
Abhishek Singh <abhishek3571@gmail.com>
2025-03-30 14:00:32 +0530

    first commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,139 @@
+
+import React, { useState } from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
+import { Link } from "react-router-dom";
+
+const pages = [
+  { name: "Home", route: "/" },
+  { name: "About", route: "/about" },
+  { name: "Skills", route: "/skills" },
+  { name: "Projects", route: "/projects" },
+  { name: "Contact", route: "/contact" },
+];
+
+const Navbar = () => {
+  const [anchorElNav, setAnchorElNav] = useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  return (
+    <AppBar position="static" sx={{ backgroundColor: "#333" }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            sx={{
+              display: { xs: "none", md: "flex" },
+              fontFamily: "cursive",
+              fontWeight: 700,
+              fontSize: 25,
+              letterSpacing: ".1rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            PORTFOLIO
+          </Typography>
+
+          {/* Mobile Menu Icon */}
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="open menu"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{ display: { xs: "block", md: "none" } }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page.name} onClick={handleCloseNavMenu}>
+                  <Typography
+                    component={Link}
+                    to={page.route}
+                    sx={{
+                      textDecoration: "none",
+                      color: "inherit",
+                      textAlign: "center",
+                    }}
+                  >
+                    {page.name}
+                  </Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+
+          <Typography
+            variant="h5"
+            sx={{
+              flexGrow: 1,
+              display: { xs: "flex", md: "none" },
+              fontFamily: "cursive",
+              fontWeight: 700,
+              fontSize: 25,
+              letterSpacing: ".1rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            PORTFOLIO
+          </Typography>
+
+          {/* Desktop Menu */}
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" }, ml:30 }}>
+            {pages.map((page) => (
+              <Button
+                component={Link}
+                to={page.route}
+                key={page.name}
+                onClick={handleCloseNavMenu}
+                sx={{
+                  mr: 2,
+                  color: "white",
+                  display: "block",
+                }}
+              >
+                {page.name}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+
+export default Navbar;
